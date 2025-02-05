ad124911f37c153363f6d241d6161caace3acdc7
ZarkaHasan9 <100154150+ZarkaHasan9@users.noreply.github.com>
2025-02-06 01:41:11 +0530

    commit

---
+++ diabetes/home/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render,HttpResponse
+
+# Create your views here.
+def index(request):
+    return HttpResponse("This is the home page ")
+def about(request):
+    return HttpResponse("This is the about us page ")
