c8074fe98d400040432b07cd4b8d8724f329ab8a
ZarkaHasan9 <100154150+ZarkaHasan9@users.noreply.github.com>
2025-06-04 18:46:39 +0530

    Configured static files and added WhiteNoise

--- diabetes/diabetes/settings.py
+++ diabetes/diabetes/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -134,4 +135,5 @@ STATICFILES_DIR = [
     os.path.join(BASE_DIR,"static"),
 ]
 STATIC_ROOT= os.path.join(os.path.dirname(BASE_DIR),'static')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
  
